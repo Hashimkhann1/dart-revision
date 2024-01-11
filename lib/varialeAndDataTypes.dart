71e57a66bb755540c2e7c9cd3970c46b0badcd49
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-11 10:29:04 +0500

    in this file i practice how to declar variables of different data types

---
+++ lib/varialeAndDataTypes.dart
@@ -0,0 +1,21 @@
+void main() {
+
+// Variable and Datatypes
+
+
+int number; // this is how we declare integir variable
+double doubleNumber; // this is how we declare double variable
+String name; // this is how we declare String variable
+bool isCorrect; // this is how we declare bool variable
+
+number = 5;
+doubleNumber = 2.2;
+name = "M Hashim";
+isCorrect = true;
+
+print(number);
+print(doubleNumber);
+print(name);
+print(isCorrect);
+
+}
