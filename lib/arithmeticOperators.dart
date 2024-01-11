d9fda51f1268e8adc5002317f340318ee0a64753
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-11 10:37:26 +0500

    In this dart file i practice how to use Arithmetic operators

---
+++ lib/arithmeticOperators.dart
@@ -0,0 +1,32 @@
+void main() {
+
+// Arithmetic operators + , - , * , /
+
+// this + opreater is used for Adding of two sumbers
+// this - opreater is used for Subtraction of two sumbers
+// this * opreater is used for Multification of two sumbers
+// this / opreater is used for Division of two sumbers
+
+int numberOne = 20;
+int numberTwo = 5;
+
+int sum;
+int subtraction;
+int multiplaction;
+double division;
+
+
+sum = numberOne + numberTwo; // Addition of two numbers
+subtraction = numberOne - numberTwo; // Subtraction of two numbers
+multiplaction = numberOne * numberTwo; // multiplaction of two numbers
+division = numberOne / numberTwo; // division of two numbers
+
+
+print(sum);
+print(subtraction);
+print(multiplaction);
+print(division);
+
+
+
+}
