dc45b31f95866fcf9eba1a4c63ba1e5c7474f6ce
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-21 23:41:19 +0500

    class is practicce

--- lib/oopsClass.dart
+++ lib/oopsClass.dart
@@ -13,12 +13,46 @@ class smartPhone{
 
 }
 
+class Mathematics {
+
+  // addation
+  int addation(int n1 ,int n2){
+    return n1 + n2;
+  }
+
+  // subtraction
+  int subtraction(int n1 ,int n2){
+    return n1 - n2;
+  }
+
+  // multiplication
+  int multiplication(int n1 ,int n2){
+    return n1 * n2;
+  }
+
+  // division
+  double division(double n1 ,double n2){
+    return n1 / n2;
+  }
+
+}
+
 
 void main() {
-  var phone1 = smartPhone();
-  phone1.color = "Yellow";
-  print(phone1.color);
-  print(smartPhone().color);
-  print(phone1.size);
-  phone1.turnOnTorch();
+
+
+  // var phone1 = smartPhone();
+  // phone1.color = "Yellow";
+  // print(phone1.color);
+  // print(smartPhone().color);
+  // print(phone1.size);
+  // phone1.turnOnTorch();
+
+  Mathematics mathematics = Mathematics();
+
+  print(mathematics.addation(2, 2));
+  print(mathematics.subtraction(6, 2));
+  print(mathematics.multiplication(3, 4));
+  print(mathematics.division(10, 2));
+
 }
