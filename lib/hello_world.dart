13b8b9c11ea9cd731dd3238541181163dcda9afb
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-21 23:40:54 +0500

    leetCode problem i solved

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,3 +1,27 @@
 void main() {
+
   print("Hello World");
+
+
+  List nums = [1,3,5,6];
+  int target = 7;
+  if(nums.contains(target)){
+    for(int i = 0; i < nums.length; i++){
+      if(nums[i] == target){
+        print(i);
+      }
+    }
+  }else{
+    for(int i = 0; i < nums.length; i++){
+      if(nums[i] > target){
+        print(i);
+        break;
+      }
+    
+    }
+  }
+
+
 }
+
+
