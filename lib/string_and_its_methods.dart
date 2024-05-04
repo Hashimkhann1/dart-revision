67f900bf5b75d497cdc7aec1d8aa4d527fe668b8
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-04 23:33:59 +0500

    i practice new methods of string

--- lib/string_and_its_methods.dart
+++ lib/string_and_its_methods.dart
@@ -1,6 +1,8 @@
 void main() {
   String name = "My name is M Hashim";
   String age = " and i'm 21 years old";
+  String test = 'testing';
+  String test2 = 'Testing';
   print(name);
 
   // strign legth
@@ -28,4 +30,10 @@ void main() {
 
   // srtring slicing
   // print(name[1,]);
+
+  print(test.endsWith('ing'));
+  // print(test.allMatches(test2));
+ print(name);
+ print(name.replaceAll('M Hashim', ' M Hashim Khan'));
+ print(name.indexOf('hom'));
 }
