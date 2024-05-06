ce7daedcb55b594d799f75fc6e6a25613b15ae2c
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-06 22:46:09 +0500

    leetcode challenge find the first occurance string in string

---
+++ lib/challengs/first_Occurrence.dart
@@ -0,0 +1,14 @@
+
+
+
+void main() {
+
+  String  haystack = "hello", needle = "ll";
+  // print(haystack.indexOf(needle));
+  if(haystack.contains(needle)){
+    print(haystack.indexOf(needle));
+  }else{
+    print(-1);
+  }
+
+}
